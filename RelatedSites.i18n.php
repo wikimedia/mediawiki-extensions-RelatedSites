76af1315d31827e2941cbbb61f4f35a128686af4
Reedy <reedy@wikimedia.org>
2012-10-12 15:53:44 +0100

    Initial commit
    Change-Id: If4cf00c91820cfde32a973359e7d9d634451f328

---
+++ RelatedSites.i18n.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Internationalisation file for RelatedSites extension.
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'rs-RelatedSites' => 'Related sites',
+	'rs-desc'         => 'Extension for presentation of related sites on the sidebar',
+);
+
+$messages['de'] = array(
+	'rs-RelatedSites' => 'Verwandte Sites',
+	'rs-desc'         => 'Erweiterung für die Ausgabe von verwandten Websites auf der Sidebar',
+);
+
+$messages['fr'] = array(
+	'rs-RelatedSites' => 'Autres sites',
+);
+
+$messages['it'] = array(
+	'rs-RelatedSites' => 'Altri siti',
+);
+
+?>
