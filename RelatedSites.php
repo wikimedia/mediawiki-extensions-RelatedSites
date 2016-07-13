bf84eba9f4ab9bfc0d77de508cb6a8b036d4aa1f
Kunal Mehta <legoktm@member.fsf.org>
2016-07-13 12:51:50 -0700

    Convert to use extension.json, remove PHP entry point
    Bug: T87966
    Change-Id: I00d15723b9e3e9a06b939f9dd6efc76679b86657

--- RelatedSites.php
+++ RelatedSites.php
@@ -1,30 +1,15 @@
 <?php
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'RelatedSites' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['RelatedSites'] = __DIR__ . '/i18n';
+	/* wfWarn(
+		'Deprecated PHP entry point used for RelatedSites extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the RelatedSites extension requires MediaWiki 1.25+' );
 }
-
-// autoloader
-$wgAutoloadClasses['RelatedSites'] = __DIR__ . '/RelatedSites.class.php';
-
-// extension & magic words i18n
-$wgMessagesDirs['RelatedSites'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['RelatedSites'] = __DIR__ . '/RelatedSites.i18n.php';
-
-// hooks
-$wgHooks['ParserBeforeTidy'][] = 'RelatedSites::onParserBeforeTidy';
-$wgHooks['OutputPageParserOutput'][] = 'RelatedSites::onOutputPageParserOutput';
-$wgHooks['SidebarBeforeOutput'][] = 'RelatedSites::onSidebarBeforeOutput';
-
-// credits
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'RelatedSites',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:RelatedSites',
-	'descriptionmsg' => 'relatedsites-desc',
-	'author' => array( 'Roland Unger', 'Hans Musil', 'Matthias Mullie' ),
-	'version' => '1.1'
-);
-
-// related interwiki prefixes that should show up on relatedsites sidebar
-$wgRelatedSitesPrefixes = array();
